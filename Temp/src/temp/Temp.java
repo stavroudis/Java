9519e40179d64d6817dc649673621f07367bb91b
stavr <stavr@STAVROS-PC>
2023-10-25 17:26:21 +0300

    leitourgei meta thn eisagwgh sxoliouss

--- Temp/src/temp/Temp.java
+++ Temp/src/temp/Temp.java
@@ -16,6 +16,8 @@ public class Temp {
     public static void main(String[] args) {
         System.out.print("geua siu");
         //adasdasdas
+        ///ffsdfsdfsdfsd
+        //fsdfsdfsd
     }
     
 }
